a82d103bdaf06221af47251bed48526dffeb255e
William Blondel <contact@williamblondel.fr>
2018-04-03 16:39:47 +0200

    lstdel now uses the del function passed as a parameter

--- src/containers/list/ft_lstdel.c
+++ src/containers/list/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: wblondel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/01 10:20:44 by wblondel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/01 10:20:44 by wblondel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/03 16:38:01 by wblondel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,7 +23,8 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	while (current != NULL)
 	{
 		tmp = current->next;
-		del(current->content, current->content_size);
+		if (del)
+			(*del)(current->content, current->content_size);
 		free(current);
 		current = tmp;
 	}
